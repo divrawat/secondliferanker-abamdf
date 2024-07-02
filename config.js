fc7764390c7cc8e1451c067fc8b472e683873deb
divrawat <divrawat2001@gmail>
2024-07-02 23:55:53 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,808 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.secondliferanker.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Second Life Ranker</u></a> follows Yeon-woo, who enters the Tower of the Sun God after discovering his twin brother Jeong-woo's death in the competition. Using Jeong-woo's diary, Yeon-woo seeks revenge, grows stronger, and uncovers hidden secrets while confronting powerful enemies and forming alliances.`;
+
+export const MANGA_NAME = 'Second Life Ranker';
+export const MANGA_AUTHOR = 'Nong Nong';
+export const MANGA_RELEASE = '2021';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Nong Nong';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Fantasy, Action, Adventure, Manhwa';
+
+export const CHAPTER_PREFIX = 'second-life-ranker-chapter';
+export const IMAGE_PREFIX = 'images/second-life-ranker';
+export const NEXT_PREVIOUS_PREFIX = 'manga/second-life-ranker-chapter';
+export const URL_PREFIX = 'manga/second-life-ranker-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/second-life-ranker-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://i.pinimg.com/736x/7c/d5/0a/7cd50a0d2dd18b6c18b268d1c63517ff.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[75px] rounded-full mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Second Life Ranker';
+export const NavbarName = "Second Life Ranker";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. Yeon-woo, who enters the Tower of the Sun God after discovering his twin brother Jeong-woo's death in the competition. Using Jeong-woo's diary, Yeon-woo seeks revenge, grows stronger, and uncovers hidden secrets while confronting powerful enemies and forming alliances.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Second Life Ranker, also known as Ranker Who Lives a Second Time, follows Yeon-woo Cha, a man who uncovers the mysterious death of his twin brother, Jeong-woo, within a treacherous competition called the Tower of the Sun God. Yeon-woo stumbles upon his brother's diary, which reveals the trials and tribulations Jeong-woo faced while ascending the tower, and provides invaluable information about its deadly challenges."
+    },
+    {
+        id: 2,
+        content: "Devastated by his brother's death and driven by a thirst for vengeance, Yeon-woo decides to enter the tower himself, determined to uncover the truth and achieve the power his brother sought. The tower is a multi-dimensional labyrinth filled with powerful beings from various worlds, all competing for ultimate power and the title of ranker. Each floor presents unique trials that test the competitors' strength, intelligence, and resolve."
+    },
+    {
+        id: 3,
+        content: "Armed with his brother's diary, Yeon-woo gains a significant advantage. The diary contains detailed accounts of the tower's structure, the abilities of other participants, and strategies to overcome the challenges. As Yeon-woo progresses, he uses this knowledge to outmaneuver his opponents and navigate the treacherous environment. His martial prowess, strategic mind, and unyielding determination make him a formidable contender."
+    },
+    {
+        id: 4,
+        content: "Throughout his journey, Yeon-woo encounters a myriad of characters, both friend and foe. He forms alliances with other climbers, such as Phante, Edora, and the enigmatic Kahn, who have their own reasons for ascending the tower. These relationships are crucial, as teamwork often becomes essential to overcoming the tower's most formidable obstacles. Despite his alliances, Yeon-woo remains vigilant, aware that trust is a rare commodity in such a ruthless competition."
+    },
+    {
+        id: 5,
+        content: "As Yeon-woo delves deeper into the tower, he discovers hidden secrets about its origin and the powerful entities that govern it. He learns about the gods who created the tower and their mysterious motives. The deeper he goes, the more he realizes that the tower is not just a place for individuals to seek power, but also a battleground for cosmic forces vying for control."
+    },
+    {
+        id: 6,
+        content: "Yeon-woo's journey is not just about revenge; it becomes a quest for self-discovery and redemption. He grapples with his own inner demons and the legacy of his brother. The tower becomes a crucible that forges him into a warrior of unparalleled skill and determination. With each floor he conquers, Yeon-woo comes closer to unraveling the truth behind his brother's death and the secrets of the tower."
+    },
+    {
+        id: 7,
+        content: "Second Life Ranker is a story of resilience, vengeance, and the unbreakable bond between brothers. It explores themes of sacrifice, power, and the lengths one will go to for justice. Set in a richly detailed world of magic and martial arts, the narrative combines intense action with intricate plot twists, making it a compelling and immersive tale of adventure and intrigue."
+
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 1
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 38
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 38
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 44
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 43
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 43
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 38
+    },
+    {
+        "chapterNumber": "65",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "66",
+        "numImages": 38
+    },
+    {
+        "chapterNumber": "67",
+        "numImages": 43
+    },
+    {
+        "chapterNumber": "68",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "69",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "70",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "71",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "72",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "73",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "74",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "75",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "76",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "77",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "78",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "79",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "80",
+        "numImages": 62
+    },
+    {
+        "chapterNumber": "81",
+        "numImages": 46
+    },
+    {
+        "chapterNumber": "82",
+        "numImages": 52
+    },
+    {
+        "chapterNumber": "83",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "84",
+        "numImages": 52
+    },
+    {
+        "chapterNumber": "85",
+        "numImages": 58
+    },
+    {
+        "chapterNumber": "86",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "87",
+        "numImages": 54
+    },
+    {
+        "chapterNumber": "88",
+        "numImages": 53
+    },
+    {
+        "chapterNumber": "89",
+        "numImages": 56
+    },
+    {
+        "chapterNumber": "90",
+        "numImages": 50
+    },
+    {
+        "chapterNumber": "91",
+        "numImages": 53
+    },
+    {
+        "chapterNumber": "92",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "93",
+        "numImages": 53
+    },
+    {
+        "chapterNumber": "94",
+        "numImages": 47
+    },
+    {
+        "chapterNumber": "95",
+        "numImages": 54
+    },
+    {
+        "chapterNumber": "96",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "97",
+        "numImages": 53
+    },
+    {
+        "chapterNumber": "98",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "99",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "100",
+        "numImages": 56
+    },
+    {
+        "chapterNumber": "101",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "102",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "103",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "104",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "105",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "106",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "107",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "108",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "109",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "110",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "111",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "112",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "113",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "114",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "115",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "116",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "117",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "118",
+        "numImages": 37
+    },
+    {
+        "chapterNumber": "119",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "120",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "121",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "122",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "123",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "124",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "125",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "126",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "127",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "128",
+        "numImages": 30
+    },
+    {
+        "chapterNumber": "129",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "130",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "131",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "132",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "133",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "134",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "135",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "136",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "137",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "138",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "139",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "140",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "141",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "142",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "143",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "144",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "145",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "146",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "147",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "148",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "149",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "150",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "151",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "152",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "153",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "154",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "155",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "156",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "157",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "158",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "159",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "160",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "161",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "162",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "163",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "164",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "165",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "166",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "167",
+        "numImages": 23
+    },
+    {
+        "chapterNumber": "168",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "169",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "170",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "171",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "172",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "173",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "174",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "175",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "176",
+        "numImages": 24
+    }
+]
