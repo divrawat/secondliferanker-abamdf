6f208c08a9a1c2cade4591c4462a4d81f3df12fb
divrawat <divrawat2001@gmail>
2024-07-13 12:13:24 +0530

    se0fse5f

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="monetag" content="1d93dc930c6a055863b5061846518137" />
+      </Head>
       <body>
         <Main />
         <NextScript />
